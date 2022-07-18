7781c844089e3653be1855bb25b64b5f3f60edde
xd003 <singhbadal04@gmail.com>
2022-07-18 23:19:05 +0530

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -34,7 +34,7 @@ sudo rm -rf $HOME/.config/hypr
 mkdir -p $HOME/hypr && wget https://staging.xd003.workers.dev/0:/Dumps/hyprland.tar.gz -P $HOME
 tar -xzvf $HOME/hyprland.tar.gz -C $HOME/hypr && rm -rf $HOME/hyprland.tar.gz
 mkdir -p ~/.config/hypr && cp -r $HOME/hypr/dotfiles/dark/* ~/.config/hypr/
-sudo mkdir -p /usr/local/share/fonts && sudo cp -r $HOME/hypr/fonts/* /usr/local/share/fonts/ && && sudo fc-cache -fv
+sudo mkdir -p /usr/local/share/fonts && sudo cp -r $HOME/hypr/fonts/* /usr/local/share/fonts/ && sudo fc-cache -fv
 sudo mkdir -p /usr/share/themes && sudo cp -r $HOME/hypr/gtk/theme/* /usr/share/themes/
 sudo mkdir -p /usr/share/icons && sudo cp -r $HOME/hypr/gtk/icons/* /usr/share/icons/
 sudo cp -r $HOME/hypr/gtk/cursor/* /usr/share/icons/
