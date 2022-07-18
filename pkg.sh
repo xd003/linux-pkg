0e91d021c4599378da27e654aecd86a3c8e52614
xd003 <singhbadal04@gmail.com>
2022-07-18 23:53:15 +0530

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -17,9 +17,9 @@ sudo nano /etc/pacman.conf
 
 #Install Pacman & AUR packages
 sudo pacman -Syu paru firefox gnome-keyring seahorse libsecret telegram-desktop flatpak xf86-video-intel xarchiver thunar thunar-archive-plugin noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation noto-fonts-emoji \
-swaybg swayidle  swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad thunar geany mpv mpd mpc viewnior imagemagick xfce-polkit xorg-xwayland
+swaybg swayidle  swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad geany mpv mpd mpc viewnior imagemagick xorg-xwayland
 paru -S 7-zip termius vscodium-bin 1password freedownloadmanager powerpill \
-hyprland-bin ly wlogout
+hyprland-bin ly wlogout xfce-polkit
 
 #Flatpak package Install
 flatpak install flathub com.github.rafostar.Clapper
