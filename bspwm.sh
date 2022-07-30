b1ffa93246ec368a71dcdbae1a18bc7ddf69e4ea
xd003 <singhbadal04@gmail.com>
2022-07-30 19:53:31 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #Install Pacman & Aur Packages
-sudo pacman -S bspwm sxhkd polybar alacritty stalonetray xfce4-power-manager rofi jgmenu dunst pavucontrol exa zsh zsh-completions dmenu nitrogen flameshot
+sudo pacman -S bspwm sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa zsh zsh-completions dmenu nitrogen flameshot
 paru -S ttf-icomoon-feather xbanish
