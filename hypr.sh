2e43ed3a679a1e8bdcc1e473950df2b7c1c65a9f
xd003 <singhbadal04@gmail.com>
2022-07-30 08:03:33 +0530

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -3,4 +3,4 @@
 #Update Packages
 sudo pacman -Syu
 
-
+sudo pacman -S swaybg swayidle  swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad mpv mpd mpc viewnior imagemagick xorg-xwayland pavucontrol qt5-wayland
