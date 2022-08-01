2ba1532d2b51c94aa211cfceca494a80aa5d50c9
xd003 <singhbadal04@gmail.com>
2022-08-01 22:54:03 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Install Pacman & Aur Packages
-sudo pacman -S bspwm xdo sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions dmenu nitrogen flameshot xsetroot
+sudo pacman -S bspwm xdo sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions dmenu nitrogen flameshot xsetroot xprintidle
 paru -S ttf-icomoon-feather xbanish
 
 git clone https://github.com/baskerville/xqp $HOME && cd $HOME/xqp && sudo make && sudo make install && rm -rf $HOME/xqp
