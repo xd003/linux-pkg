824865a45bf9356c7b9526a2dc130ede52963599
xd003 <singhbadal04@gmail.com>
2022-08-02 13:27:53 +0530

    Update base.sh

--- base.sh
+++ base.sh
@@ -13,8 +13,8 @@ read -n 1 -s -r -p "Press any key to continue: "
 sudo nano /etc/pacman.conf
 
 #Install Pacman & AUR packages
-sudo pacman -Syu paru firefox geany viewnior xdg-user-dirs gnome-keyring seahorse libsecret telegram-desktop flatpak xf86-video-intel intel-media-driver libva-intel-driver libva-utils intel-gpu-tools xarchiver celluloid-git thunar thunar-archive-plugin thunar-volman noise-suppression-for-voice noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation noto-fonts-emoji ttf-roboto papirus-icon-theme kvantum qt5ct lxappearance 
-paru -S 7-zip termius vscodium-bin 1password freedownloadmanager powerpill ventoy-bin ly nerd-fonts-jetbrains-mono orchis-theme nextdns
+sudo pacman -Syu paru firefox geany viewnior xdg-user-dirs gnome-keyring seahorse libsecret telegram-desktop flatpak xf86-video-intel intel-media-driver libva-intel-driver libva-utils intel-gpu-tools xarchiver celluloid-git thunar thunar-archive-plugin thunar-volman noise-suppression-for-voice noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation noto-fonts-emoji ttf-roboto papirus-icon-theme kvantum qt5ct lxappearance bspwm xdo sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa bat duf zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions dmenu nitrogen flameshot xsetroot xprintidle xss-lock
+paru -S 7-zip termius vscodium-bin 1password freedownloadmanager powerpill ventoy-bin ly nerd-fonts-jetbrains-mono orchis-theme nextdns ttf-icomoon-feather xbanish
 
 #Start NextDNS Service
 sudo nextdns install -config bd435d -report-client-info -auto-activate
@@ -24,9 +24,5 @@ sudo nextdns install -config bd435d -report-client-info -auto-activate
 
 #Enable Display Manager
 sudo systemctl enable ly
-
-#Install Pacman & Aur Packages
-sudo pacman -S bspwm xdo sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa bat duf zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions dmenu nitrogen flameshot xsetroot xprintidle xss-lock
-paru -S ttf-icomoon-feather xbanish
 
 git clone https://github.com/baskerville/xqp $HOME && cd $HOME/xqp && sudo make && sudo make install && rm -rf $HOME/xqp
