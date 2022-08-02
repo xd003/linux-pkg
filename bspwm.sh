cc97e6050b70b57e34211c63d9b10ce02bacf367
xd003 <singhbadal04@gmail.com>
2022-08-02 13:19:14 +0530

    Delete bspwm.sh

--- bspwm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#Install Pacman & Aur Packages
-sudo pacman -S bspwm xdo sxhkd polybar alacritty xfce4-power-manager rofi jgmenu dunst pavucontrol exa bat duf zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions dmenu nitrogen flameshot xsetroot xprintidle xss-lock
-paru -S ttf-icomoon-feather xbanish
-
-git clone https://github.com/baskerville/xqp $HOME && cd $HOME/xqp && sudo make && sudo make install && rm -rf $HOME/xqp
