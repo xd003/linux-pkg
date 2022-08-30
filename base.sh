42fc87e01dc37d8979ed5f66c25c08f77a3430e5
xd003 <singhbadal04@gmail.com>
2022-08-30 14:43:56 +0530

    Update base.sh

--- base.sh
+++ base.sh
@@ -17,9 +17,9 @@ sudo pacman -Syu --noconfirm paru firefox viewnior xdg-user-dirs xdg-utils gnome
 libva-utils intel-gpu-tools grub-customizer \
 noise-suppression-for-voice exa bat duf unzip zsh reflector zathura zathura-pdf-mupdf neovim wget aria2 fd ffmpeg plocate \
 noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation noto-fonts-emoji papirus-icon-theme kvantum qt5ct lxappearance \
-bspwm sxhkd polybar alacritty xdo xfce4-power-manager rofi jgmenu dunst pavucontrol pamixer dmenu nitrogen flameshot xorg-xsetroot xprintidle maim python-gobject neofetch fontconfig freetype2 xorg-mkfontscale xorg-mkfontdir xf86-input-libinput
+bspwm sxhkd polybar alacritty xdo xfce4-power-manager rofi jgmenu dunst pavucontrol dmenu nitrogen flameshot xorg-xsetroot xprintidle maim python-gobject neofetch fontconfig freetype2 xorg-mkfontscale xorg-mkfontdir xf86-input-libinput
 
-paru -S 7-zip termius vscodium-bin 1password freedownloadmanager ventoy-bin ly nerd-fonts-jetbrains-mono orchis-theme nextdns ttf-icomoon-feather xbanish anydesk-bin touchegg betterlockscreen xtitle ttf-ms-fonts devour
+paru -S 7-zip termius vscodium-bin 1password freedownloadmanager ventoy-bin ly nerd-fonts-jetbrains-mono orchis-theme nextdns ttf-icomoon-feather xbanish anydesk-bin touchegg betterlockscreen xtitle ttf-ms-fonts devour ksuperkey
 #Start NextDNS Service
 sudo nextdns install -config 9897ea -report-client-info -auto-activate
 
