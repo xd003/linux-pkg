69a0af11fc5253aa9ac6cff9c81596997e6f4158
xd003 <singhbadal04@gmail.com>
2022-09-09 18:01:16 +0530

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -22,6 +22,7 @@ sudo systemctl enable ly
 sudo systemctl enable touchegg.service
 systemctl enable betterlockscreen@xd003
 sudo systemctl enable systemd-oomd
+sudo systemctl enable auto-cpufreq
 
 mkdir $HOME/xqp && git clone https://github.com/baskerville/xqp $HOME/xqp && cd $HOME/xqp && sudo make && sudo make install && rm -rf $HOME/xqp
 ## wget WALLS_URL && betterlockscreen -u /home/xd003/Pictures/Wallpapers/forest.png
