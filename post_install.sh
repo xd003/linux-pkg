bd76a6061460c61d5892a522b72a30e43e4243e2
xd003 <singhbadal04@gmail.com>
2022-11-13 10:51:46 +0530

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-#Add Chaotic AUR
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && echo
-echo && cat << EOF
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
-EOF
-echo && echo "Copy the above text & paste in /etc/pacman.conf , also uncomment parallel downloads(15) & multilib"
-read -n 1 -s -r -p "Press any key to continue: "
-sudo nano /etc/pacman.conf
-
 # Install pkg
 
 #Start NextDNS Service
