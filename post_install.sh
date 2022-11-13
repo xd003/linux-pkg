8fe8431f6a9f1418e5fa9a5278f9f45c467c852a
xd003 <singhbadal04@gmail.com>
2022-11-13 13:22:18 +0530

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-# Install pkg
-
 #Start NextDNS Service
 sudo nextdns install -config 9897ea -report-client-info -auto-activate
 
@@ -14,7 +11,3 @@ sudo systemctl enable auto-cpufreq
 
 mkdir $HOME/xqp && git clone https://github.com/baskerville/xqp $HOME/xqp && cd $HOME/xqp && sudo make && sudo make install && rm -rf $HOME/xqp
 ## wget WALLS_URL && betterlockscreen -u /home/xd003/Pictures/Wallpapers/forest.png
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-mkdir -p $HOME/.cache/zsh
-sudo updatedb
