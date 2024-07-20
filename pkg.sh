705d7c4d348387f8c7dbb241aa3f6e73ec1da6bc
xd003 <singhbadal04@gmail.com>
2024-07-20 20:49:31 +0530

    Create pkg.sh

---
+++ pkg.sh
@@ -0,0 +1,28 @@
+# Intel Graphic Drivers
+mesa 
+libva-intel-driver 
+intel-media-driver 
+vulkan-intel 
+libva-utils 
+intel-gpu-tools
+
+# Gnome
+gnome-shell 
+gnome-control-center
+polkit-gnome
+gnome-keyring
+
+# Display_manager
+gdm 
+
+# Bluetooth
+bluez
+
+# File Manager
+nemo
+
+# Terminal
+foot
+
+# General Apps
+firefox
